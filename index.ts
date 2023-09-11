99599f26df6c37c63ef40475013ffdf8e4e047c5
Juan Campa <juancampa@gmail.com>
2023-09-11 16:49:27 -0400

    Args codemod

--- index.ts
+++ index.ts
@@ -3,26 +3,26 @@
 // `state` is an object that persists across program updates. Store data here.
 import { nodes, root, state } from 'membrane';
 
-export async function sum({ args: { a, b } }) {
+export async function sum({ a, b }) {
   return a + b;
 }
 
-export async function multiply({ args: { a, b } }) {
+export async function multiply({ a, b }) {
   return a * b;
 }
 
-export async function subtract({ args: { a, b } }) {
+export async function subtract({ a, b }) {
   return a - b;
 }
 
-export async function divide({ args: { a, b } }) {
+export async function divide({ a, b }) {
   if (b === 0) {
     throw new Error("Division by zero is not allowed.");
   }
   return a / b;
 }
 
-export async function sqrt({ args: { a } }) {
+export async function sqrt({ a }) {
   if (a < 0) {
     throw new Error("Square root of a negative number is not allowed.");
   }
